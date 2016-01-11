5414ef00d66f27df7b90c0716fb3e4c37ca222f1
Dhriti Shikhar <dhrish20@gmail.com>
2016-01-11 12:55:29 +0530

    userquery in list

--- HashMash/app.py
+++ HashMash/app.py
@@ -1,15 +1,25 @@
-from  flask import Flask, render_template, url_for, request
+from  flask import Flask, render_template, url_for, request, redirect
 from flask_wtf import Form
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-  return render_template("index.html")
+  return render_template("main.html")
+
+@app.route('/', methods=['POST'])
+def entry():
+  hashes = list()
+  userquery = request.form['user-query']
+  if " " in userquery:
+    hashes = userquery.split(" ") 
+
+  if request.method == 'POST':
+    return redirect(url_for('index'))
 
 @app.route("/index")
 def index():
-  return render_template("index.html")
+  return render_template("index.html") 
 
 if __name__ == "__main__":
   app.run()
